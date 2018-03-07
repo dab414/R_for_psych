eab735b1ede598f786c43f9959b2b53f178c45d4
David Braun <dab414@lehigh.edu>
2018-03-07 10:35:45 -0500

    First substantial commit.

---
+++ Class 1 - Introduction/class1_notes_updated.r
@@ -0,0 +1,140 @@
+### SESSION 1 -- BASIC DATA MANEUVERS
+
+### Getting the hang of the R workflow can be a little tricky, and I imagine this is the first thing I'll go over in class
+
+### R AS A CALCULATOR
+
+	## This is similar to any programming lanugage, where you have basic operations that will do predictable things
+
+		1 + 1 #returns 2; all other basic operations work similarly (-, /, *) 
+    a <- 1 + 1
+		
+	## Assignment, how we tell R to store a variable using the weird '<-' assignment operator
+
+		a <- 5 # a now equals five, this won't return any output, but you'll see in the environment window that a is now stored as a variable
+		b <- 6
+		a + b # returns 11, pretty predictable
+
+### VECTORS
+	# Understanding vector logic is CRUCIAL 
+	# You can kind of think of this like a column in a dataset, it contains a bunch of (usually similar types of) data
+
+	# Initializing a simple vector:
+	x <- c(5, 4, 3, 2, 1) # c just means concatenate, it's Rs way of knowing that you want to treat each number as a separate elemenet within a vector
+	x # outputs the vector to the console
+	x <- 5:1 # is the same thing as above
+
+	x * 2 # returns a vector with the same length of x where each element is multiplied by 2
+			# this basic idea becomes critical when you want to compute a new variable as a function of an existing variable times another, for example
+			# notice the variable x stays the same as it was before, it only changes in memory if we assign the output to itself, like:
+	x <- x * 2 # NOW x is updated, but let's keep it how it was for now
+	x <- x / 2
+
+	## INDEXING VECTORS
+		# the logic of indexing is so weird and so important
+		# you use square brackets [] for immediately after the variable name for indexing any data object
+		x[2] # returns the second element of variable x, in this case the element 4
+
+		## you can also return a slice of a vector, using the ':' symbol
+		x[2:4] # returns a vector containing the second through the fourth elements 
+
+		## you can also index a vector with a vector (it's starting to get weird)
+		x[c(1,2,4)] # returns the first, second, and fourth element of vector x, and it returns this as a vector
+		## it would be the same thing as going like this:
+		y <- c(1,2,4)
+		x[y] ## but now you can see how the syntax can get all tricky and nested
+
+		## you can also index with what's called a logical, basically passing a True/False statement and it returns all elements of the vector that are 'True'
+		x[x>2] # returns '5,4,3'; notice how you have to refer to the vector again inside the vector
+
+		## you can also pass multiple conditionals with & (and), | (or)
+		x[x>2 & x<5] ## notice how you have to refer to x AGAIN on the other side of the logical joiner (the &)
+    
+		## this is super useful for trimming datasets according to some criterion, like remove all rows from the data where the 'error' variable equals TRUE (indicating there was an error)
+		## but you could index a vector by any logical, for example:
+		x <- c('red','blue','green','orange','purple')
+		y <- 15:19
+		x[x>17] 
+		## this will return '2, 1', why?
+
+	## vectors are so crucial because, for certain analyses, you'll need to refer to vectors inside a dataset to tell the function what to analyze or how to trim something
+		## the more comfortable you are with the logic of vectors, the more your unlimited analysis power is unleashed
+
+	## TYPES OF VECTORS (i.e., variables)
+
+		s <- c('a','b','c') # a character vector
+		f <- c('red','red','red','blue','blue','blue') # a factor vector
+		n <- c(1,2,3) # a numeric vector
+
+		## notice f and s are the same in the sense that they're both vectors storing characters, but let's say we want one to act as a factor
+		## you can check what 'type' an object is by using either of the following
+		typeof(f)
+		class(f)
+		## this tells us that f is actually a character vector, not good
+			## i honestly can't really tell you the difference between these two, but just in general try both if one isn't making sense
+		## but we can change variable types by:
+		f <- as.factor(f) ## also as.character(), and as.numeric(), among others
+
+		## now doing our checks gives us a different answer:
+		typeof(f)
+		class(f)
+			## see how typeof() returns integer... who knows
+
+
+
+### general functions that are handy for working with data objects
+
+	summary(f) ## returns detail about whatever object you give it
+	str(f) ## same idea, but focuses on the types of variables in any given object
+	head(f) ## returns the first few entries of an object, useful for looking at the first few rows of a large dataset
+	hist(x) ## returns a quick histogram of a vector, the object that you pass to it must be a numeric vector (wouldn't make sense otherwise)
+
+
+
+## im gonna freestyle going over data.frame(), which is actually the most useful concept for us psychologists
+## but once you master vectors the logic of data.frame() is WAY easier
+	
+library(MASS)
+	
+df <- ToothGrowth
+summary(df)
+
+#  df[rows , columns]
+df[3,2]
+
+df[5:10,1]
+df$len
+df$len[5]
+
+df[5,1]
+
+## different ways of getting same information
+df$len[5:10]
+df[5:10, 1]
+df[5:10,]
+
+df[df$len > 10,]
+
+# return all columns by leaving blank after comma
+df2 <- df[df$len > 10, ]
+
+## coding a new variable
+
+df$len_c <- df$len - mean(df$len)
+
+df$lenX2[3] <- 10
+
+colnames(df)[4] <- 'new_var'
+
+## ifelse(conditional, iftrue, if_false)
+df$len_d <- ifelse(df$len > median(df$len), 1, 0)
+df$len_d <- ifelse(df$len > 10, 2, ifelse(df$len > 4 & df$len < 10, 1, 0))
+
+
+## for help, help()
+help(ifelse)
+?ifelse()
+
+
+
+	
---
+++ Class 2 -- Manipulating Data/class2_notes.r
@@ -0,0 +1,220 @@
+#### CLASS 2
+
+## We're going to cover packages, working directories, importing data, data.table, and dplyr... or we'll at least try
+
+## TOPIC 1: PACKAGES
+	## a package is essentially a bunch of code someone else has written to make your life in R easier.
+	## remember last week when (for your exercise) i told you to click 'source on save' in the script you were writing?
+	## that's essentially what's happening when you load a package -- R is loading scripts that contain functions that you can use to do cool things
+		## for example, R has a lot of plotting functions built in, but everyone knows that all the cool plotting functions are in a package that you need to load
+		## once you load this package, you can use these functions to make beautiful graphs that everyone will love
+	## you only need to install packages once, but you need to load packages every time you start up R
+
+	# for installing a package, run this code:
+		install.packages('package') # where you replace the word package with the name of the package you want to install (but keep the quotes!)
+	# try this:
+		install.packages('data.table')
+	# then load it like this:
+		library(data.table) # notice you don't need the quotes anymore
+	# to get more information about a package, go like this:
+		help(data.table) # this help page will prob be confusing at first, but that's okay cuz that's what I'm here for
+
+	# install and load the following packages: dplyr, ggplot2
+
+	# your code here
+
+		
+		
+## TOPIC 2: WORKING DIRECTORIES
+	# when you're typing commands in your R console and you want R to interact with the files on your computer, R is always 'operating' out of a certain folder on your hard drive
+	# to see what that folder is right now, run:
+	getwd()
+	# in RStudio, you can easily change this by pressing ctl + shift + h, idk what it is for mac, sorry. but you can navigate the menus
+	# or you can do this:
+	setwd('/desired/working/directory') #obv change the text to a folder on your file system, using forward slashes EVEN on windows
+
+	# but the truly dope way to set working directories is by using projects, and I'll do this live
+
+## TOPIC 3: LOADING DATA
+	# loading data becomes much easier to understand once you grasp the concept of working directories
+	# if i tell R to load data, it's much easier to tell R where to look for the data on my file system if I tell it where to look relative to R's working directory
+	# it's the difference between this (easier) way:
+	'data/current_data.csv'
+	# and this (hard) way:
+	'C:/garbage/garbage/more_garbage/even_more_garbage/data/current_data.csv'
+
+	# there are plenty of functions for loading data, and I'm copy and pasting in some of the main ones:
+
+	## READ.TABLE(), a very common way to handle lots of different data types
+		# first row contains variable names, comma is separator 
+		# assign the variable id to row names
+		# note the / instead of \ on mswindows systems 
+
+		mydata <- read.table("mydata.csv", header=TRUE, 
+		  	sep=",", row.names="id")
+
+		# there is also read.csv(), which is faster to type if your data is a csv
+		mydata <- read.csv('mydata.csv', header = TRUE)
+
+	## FROM EXCEL
+		# read in the first worksheet from the workbook myexcel.xlsx
+		# first row contains variable names
+		library(xlsx)
+		mydata <- read.xlsx("c:/myexcel.xlsx", 1)
+
+		# read in the worksheet named mysheet
+		mydata <- read.xlsx("c:/myexcel.xlsx", sheetName = "mysheet")
+
+	## FROM SPSS
+		# save SPSS dataset in trasport format (THIS IS SPSS SYNTAX)
+			get file='c:\mydata.sav'.
+			export outfile='c:\mydata.por'. 
+
+			# in R 
+			library(Hmisc)
+			mydata <- spss.get("c:/mydata.por", use.value.labels=TRUE)
+			# last option converts value labels to R factors
+
+	## FROM SAS
+		# save SAS dataset in trasport format (THIS IS SAS SYNTAX)
+		libname out xport 'c:/mydata.xpt';
+		data out.mydata;
+		set sasuser.mydata;
+		run;
+
+		# in R 
+		library(Hmisc)
+		mydata <- sasxport.get("c:/mydata.xpt")
+		# character variables are converted to R factors
+
+	## FROM STATA
+		# input Stata file
+		library(foreign)
+		mydata <- read.dta("c:/mydata.dta")
+
+	## FROM SYSTAT
+		# input Systat file
+		library(foreign)
+		mydata <- read.systat("c:/mydata.dta")
+
+##### MY PERSONAL FAV WAY OF IMPORTING DATA
+	# comes from data.table(), called fread()
+
+	mydata <- fread('mydata.csv')
+
+	# this one performs much faster than the others (good for big data), it tends to handle data correctly by default, and has a ton of options if you wanna mess: 
+		#https://www.rdocumentation.org/packages/data.table/versions/1.10.4-2/topics/fread
+
+### TOPIC 4: SUMMARIZING DATA WITH DATA.TABLE() and DPLYR()
+
+  ## last week we talked about working with data frames as the most relevant way for us psychologists to think about manipulating data in R
+  ## now we're introducing two packages that are specialially specially designed for making working with data easier
+  ## these packages are also primo for working with big data
+  ## more than you ever wanted to know about dplyr vs data.table:
+    # https://github.com/Rdatatable/data.table/wiki/Benchmarks-:-Grouping
+    #TLDR: data.table is slightly faster, dplyr has more readable code
+
+  ## we're often in a position where we need to summarize across subjects and then summarize across conditions to get cell means
+  ## and we need to do this over and over for all different possible ways of looking at data
+  ## data.table and dplyr both provide easy ways of doing this
+
+  # read data
+  current_data <- fread('http://lehigh.edu/~dab414/easy_clean.csv')
+
+  # check things out
+  head(current_data)
+  str(current_data)
+
+  # by running str, i can see that my factors 'current' and 'other' are strings, and i need them as factors:
+  current_data$current <- as.factor(current_data$current)
+  current_data$other <- as.factor(current_data$other)
+
+  # same with subject
+  current_data$subject <- as.factor(current_data$subject)
+
+  ## we'll start with data.table(), bc it's very similar to data.frame()
+  ## basically:
+
+    # dt[rows, columns, by = ]
+    # so same as data frame, except now there's this third 'by' argument that we can use to summarize by groups
+    # also (in the columns argument) we're telling data.table what operations to perform when summarizing
+      # whereas in data.frame, this argument is only used for telling data.frame which columns we're interested in grabbing
+    # one weird quirck about DT is that (when you're passing more than one var to a single argument) data.table wants it to be passed in a list
+      # so instead of c(), we use .(), idk why
+
+  ## so to get means for my factors by subject (transcode is my dv, btw)
+  
+    # (maybe quickly demo how to do it in data.frame, the DUMB way)
+
+    #convert to data table
+    current_data <- as.data.table(current_data)
+    # make new dataset with subject means (note im leaving the 'row' entry blank because i don't want to filter out rows)
+    subject_means <- current_data[, .(switch = mean(transcode)), by = .(subject, current, other)]
+    # look at the data
+    subject_means # note data.table is smart and won't assault you by outputing the entire dataset if it's too big
+  
+    # how would you compute subject means (just like above) but only keep data that occurs after block 5?
+
+      # your code here
+
+    
+    
+    
+    # to condense to cell means:
+      cell_means <- subject_means[, .(switch = mean(switch)), by = .(current, other)]
+
+    # cell means with SEs:
+      cell_means <- subject_means[, .(switch = mean(switch), se = sd(switch) / sqrt(.N)), by = .(current, other)]      
+
+
+  ## doing the same thing with dplyr
+  # important functions are, group_by(), summarize()
+
+    subject_means <- current_data %>%
+      group_by(subject, current, other) %>%
+      summarize(transcode = mean(transcode)) %>%
+      ggplot()
+
+    cell_means <- subject_means %>%
+      group_by(current, other) %>%
+      summarize(switch = mean(transcode), se = sd(transcode) / sqrt(n()))
+
+  ## data table and dplyr can both do these in one step
+
+    # data table
+      cell_means <- current_data[, .(switch = mean(transcode)), by = .(subject, current, other)][, .(switch = mean(switch), se = sd(switch) / sqrt(.N)), by = .(current, other)][order(current,other)]
+    
+    # dplyr
+      cell_means <- current_data %>%
+        group_by(subject, current, other) %>%
+        summarize(transcode = mean(transcode)) %>%
+        group_by(current, other) %>%
+        summarize(switch = mean(transcode), se = sd(transcode) / sqrt(n()))
+
+## A NOTE OF WARNING
+
+  ## indexing a data table or 'tibble' (as it's returned in dplyr) doesn't always return a vector like data.frame
+
+    #compare
+
+    current_data_df <- as.data.frame(current_data)
+
+    # data frame return type
+    current_data_df[1:5, 'current']
+    # data table
+    current_data[1:5, 'current']
+    # dplyr
+    subject_means[1:5, 'transcode']
+
+  ## this doesn't usually matter, but it's good to be aware of
+  ## to get a vector back from data table or dplyr, use the dollar sign indexing
+    current_data$current[1:5]
+    
+    
+    
+    
+    
+    
+    
+    
+    
